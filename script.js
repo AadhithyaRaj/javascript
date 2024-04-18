70824240ea68aac36745deb04199f6238dfd4474
AadhithyaRaj <aadhithya3210@gmail.com>
2024-04-18 15:13:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function evenOrOdd(){
+    let val = document.getElementById('text1');
+    if(val%2===1){
+        document.getElementById('text1output').innerHTML = "Odd number";
+    }
+    else{
+        document.getElementById('text1output').innerHTML = "Odd number";
+    }
+}
